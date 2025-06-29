9be1954761bf13aa059adfc1cb41b86111cad9a4
theRealTKD <tkaandilmac@hotmail.com>
2025-06-29 19:55:51 +0300

    adding master hunter with hunter trigger
    the guy who made the vanilla remade mod just answered it . i fell blessed

--- MyFirstMod.lua
+++ MyFirstMod.lua
@@ -79,6 +79,7 @@ SMODS.Joker{
 	end,
     calculate = function (self,card,context)
         if context.joker_main then
+            SMODS.add_card{key = "j_my_master_hunter"}
 			return {
 				chip_mod = card.ability.extra.chips,
 				message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } }

78f3f3b21d809f7c5bea7391ad5aa8f5db08d54d
theRealTKD <tkaandilmac@hotmail.com>
2025-07-06 07:59:30 +0300

    last asset fix

--- SoytarI.lua
+++ SoytarI.lua
@@ -8,52 +8,6 @@ SMODS.Atlas {
 	-- Height of each sprite in 1x size
 	py = 95
 }
-
---Self-portrait
---#region
---prepairing photograph joker for self_portrait
-SMODS.Joker:take_ownership('photograph',{
-    add_to_deck = function(self, card, from_debuff)
-		G.GAME.pool_flags.photograph_bought = true --if a pool flag is not defined it returns false.
-	end
-    },
-    true--when set to true it makes my change silent. so no mod stamp.
-)
-
-
-SMODS.Joker{
-    key = 'self_portrait',
-    loc_txt = {
-        name = 'Self-Portrait',
-        text = {
-            "Played{C:attention} face cards{} give",
-            "{X:mult,C:white}x#1#{} Mult when scored"
-            }
-    },
-    in_pool = function(self, args)
-        return G.GAME.pool_flags.photograph_bought -- photograph_bought is not defined until photograph is added to deck so it returns false
-    end,
-    blueprint_compat = true,
-    config = {extra = {xmult = 1.50} },
-    rarity = 3,
-    atlas = 'SoytarI',
-    pos = {x = 0,  y = 0},
-    cost = 8,
-    loc_vars = function(self,info_queue,card)
-        return { vars = { card.ability.extra.xmult}}
-    end,
-    calculate = function (self,card,context)
-        if context.cardarea == G.play and context.individual and context.other_card:is_face() then
-			return {
-				Xmult_mod = card.ability.extra.xmult,
-				--card = context.other_card,
-                message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.xmult } }
-			}
-		end
-    end
-}
---#endregion
-
 -- HUNTER
 --#region
 SMODS.Joker{
@@ -110,51 +64,6 @@ SMODS.Joker{
 			}
 		end
         --
-    end
-}
---#endregion
-
---MASTER HUNTER
---#region
-SMODS.Joker{
-    key = 'master_hunter',
-    loc_txt = {
-        name = 'Master Hunter',
-        text = {
-        "Earn {C:money}$#1#{} after",
-        "beating a {C:attention}boss blind{}",
-        "{C:chips}+#2#{} chips and {X:mult,C:white}x#3#{} Mult on",
-        "{C:attention}boss blinds{}"
-    }
-    },
-    config = {extra = {money = 12, chips = 120, xmult = 2}},
-    rarity = 3,
-    blueprint_compat = true,
-    in_pool = function(self, args)
-        return false
-    end,
-    atlas = 'SoytarI',
-    pos = {x = 0, y = 1},
-    soul_pos = {x=1,y=1},
-    cost = 10,
-    loc_vars = function(self,info_queue,card)
-        return { vars = { card.ability.extra.money, card.ability.extra.chips, card.ability.extra.xmult}}
-    end,
-    --
-    calc_dollar_bonus = function(self, card)
-        if G.GAME.last_blind and G.GAME.last_blind.boss then
-		local bonus = card.ability.extra.money
-		if bonus > 0 then return bonus end
-        end
-	end,
-    calculate = function (self,card,context)
-        if context.joker_main and G.GAME.last_blind.boss then
-			return {
-				chips = card.ability.extra.chips,
-				Xmult = card.ability.extra.xmult,
-				--message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } }
-			}
-		end
     end
 }
 --#endregion
@@ -252,6 +161,96 @@ SMODS.Joker{
                     xmult = card.ability.extra.Xmult
                 }
         end
+    end
+}
+--#endregion
+
+--Self-portrait
+--#region
+--prepairing photograph joker for self_portrait
+SMODS.Joker:take_ownership('photograph',{
+    add_to_deck = function(self, card, from_debuff)
+		G.GAME.pool_flags.photograph_bought = true --if a pool flag is not defined it returns false.
+	end
+    },
+    true--when set to true it makes my change silent. so no mod stamp.
+)
+
+
+SMODS.Joker{
+    key = 'self_portrait',
+    loc_txt = {
+        name = 'Self-Portrait',
+        text = {
+            "Played{C:attention} face cards{} give",
+            "{X:mult,C:white}x#1#{} Mult when scored"
+            }
+    },
+    in_pool = function(self, args)
+        return G.GAME.pool_flags.photograph_bought -- photograph_bought is not defined until photograph is added to deck so it returns false
+    end,
+    blueprint_compat = true,
+    config = {extra = {xmult = 1.50} },
+    rarity = 3,
+    atlas = 'SoytarI',
+    pos = {x = 0,  y = 0},
+    cost = 8,
+    loc_vars = function(self,info_queue,card)
+        return { vars = { card.ability.extra.xmult}}
+    end,
+    calculate = function (self,card,context)
+        if context.cardarea == G.play and context.individual and context.other_card:is_face() then
+			return {
+				Xmult_mod = card.ability.extra.xmult,
+				--card = context.other_card,
+                message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.xmult } }
+			}
+		end
+    end
+}
+--#endregion
+
+--MASTER HUNTER
+--#region
+SMODS.Joker{
+    key = 'master_hunter',
+    loc_txt = {
+        name = 'Master Hunter',
+        text = {
+        "Earn {C:money}$#1#{} after",
+        "beating a {C:attention}boss blind{}",
+        "{C:chips}+#2#{} chips and {X:mult,C:white}x#3#{} Mult on",
+        "{C:attention}boss blinds{}"
+    }
+    },
+    config = {extra = {money = 12, chips = 120, xmult = 2}},
+    rarity = 3,
+    blueprint_compat = true,
+    in_pool = function(self, args)
+        return false
+    end,
+    atlas = 'SoytarI',
+    pos = {x = 0, y = 1},
+    soul_pos = {x=1,y=1},
+    cost = 10,
+    loc_vars = function(self,info_queue,card)
+        return { vars = { card.ability.extra.money, card.ability.extra.chips, card.ability.extra.xmult}}
+    end,
+    --
+    calc_dollar_bonus = function(self, card)
+        if G.GAME.last_blind and G.GAME.last_blind.boss then
+		local bonus = card.ability.extra.money
+		if bonus > 0 then return bonus end
+        end
+	end,
+    calculate = function (self,card,context)
+        if context.joker_main and G.GAME.last_blind.boss then
+			return {
+				chips = card.ability.extra.chips,
+				Xmult = card.ability.extra.xmult,
+				--message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips } }
+			}
+		end
     end
 }
 --#endregion
